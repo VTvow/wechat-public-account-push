af94ef0b994afa40cadca8d4571596aeb01446db
liangyongning <824221253@qq.com>
2023-03-23 22:33:06 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ovrK55oh1bRgiS8dp8JnRdAB-4u0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'GH1lPgvnmbk4i_i1GDzxntIHygGAjaW7Alrkl1BAEPg',
+      useTemplateId: 'cu2BBwdVVmLu05lx2f1OCJmlTpN-u-PGdVxtHn3KsHE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-24',
       festivals: [
@@ -54,14 +54,38 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'GH1lPgvnmbk4i_i1GDzxntIHygGAjaW7Alrkl1BAEPg',
+  CALLBACK_TEMPLATE_ID: 'DKVDHPqk-_P_Eby8owy-sj2uDsZd_4HTbUyeuaFMwMc',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: '小宁',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ovrK55ha2b3EJjVtlxk7QRECg-wc',
     }
+     horoscopeDate: '01-20',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        //{
+         // type: '*生日', name: '居居', year: '1995', date: '10-03',
+       //},
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+       // {
+         // type: '生日', name: '居居', year: '1995', date: '11-24',
+        //},
+        {
+          type: '生日', name: '小宁', year: '1995', date: '01-20',
+        },
+        //{
+         // type: '节日', name: '相识纪念日', year: '2022', date: '06-20',
+        //},
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      //customizedDateList: [
+        // 在一起的日子
+       // { keyword: 'love_day', date: '2022-06-20' },
+        // 结婚纪念日
+        //{ keyword: 'marry_day', date: '2022-09-09' },
+      ],
   ],
 
 }
