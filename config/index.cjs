1909406717902d7a4ab0fdfd56393a709ee1300b
liangyongning <824221253@qq.com>
2023-03-23 23:01:21 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -55,7 +55,7 @@ const USER_CONFIG = {
   USERS: [
 	    {
 	      // 想要发送的人的名字
-	      name: '小宁,
+	      name: '小宁',
 	      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
 	      id: 'ovrK55ha2b3EJjVtlxk7QRECg-wc',
 	      // 使用微信测试号：你想对他发送的模板消息的模板ID
